4735ddf7a89b81fecd41e10220dd25de91abe8ca
Daniel Lee <daniellegomaniac@gmail.com>
2018-04-08 18:00:48 -0700

    Add hw02

---
+++ Homeworks/hw02/tests/q6_2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 6_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(inventory.sort(0).column(0).item(0), int)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
