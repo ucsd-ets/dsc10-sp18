e86961d74852a0e1b1cd41fbb47ad75e5c161e97
Casey Duong <ckduong14@gmail.com>
2018-04-15 15:05:42 -0700

    Updated HW3

---
+++ Homeworks/hw03/tests/q1_2.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 1_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(by_nei, Table)
+          True
+          >>> isinstance(by_nei_pter, Table)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
