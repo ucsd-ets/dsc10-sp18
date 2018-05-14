7e764a24104d40725f3579108004b831a498d772
Casey Duong <ckduong14@gmail.com>
2018-05-14 00:18:40 -0700

    Added HW7

---
+++ Homeworks/hw07/tests/q1_2.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 1_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(inSampleV1(make_array([1,3,5]), 4), bool)
+          True
+          >>> isinstance(is_six_in1, bool)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ Homeworks/hw07/tests/q4_2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 4_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> estimate_pop(100, 100, 1) > 0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
