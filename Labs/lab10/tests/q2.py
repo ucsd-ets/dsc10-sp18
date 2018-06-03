1be9520683e6ee0540459905415b08ff66af52ec
Parker <pargrey@gmail.com>
2018-06-03 11:21:58 -0700

    Added lab10

---
+++ Labs/lab10/tests/q2.py
@@ -0,0 +1,46 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 0 < arrows < 5
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> arrows != 1
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> arrows != 2
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> arrows != 4
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
